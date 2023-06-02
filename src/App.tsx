7921445fc5d425d0e08177a41fd6da46f5b49479
luanamsilva <luanamoura2002@yahoo.com.br>
2023-06-02 10:25:32 -0300

    add-style-link

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,20 @@
-import RoutesApp from './routes'
-import {useEffect, useState} from 'react'
-import api from './services/api'
-import styles from './App.module.css'
-import { Link } from "react-router-dom";
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import {Header} from './components/Header'
+import Home from './pages/Home'
+import Movie from './pages/Movie'
 
 
-function App() {
-
-  const [movies, setMovies] = useState([])
-
-  useEffect(()=>{
-    async function loadMovies() {
-     const response = await api.get("movie/now_playing", {
-      params: {
-      api_key: "52139d7800b359f24813bae453e23e2a",
-     language: "pt-BR",
-     page: 1,
-     }
-   })
-   setMovies(response.data.results)
-  }
-    loadMovies();
-  },[])
-
-  return (
-    <> 
-      <RoutesApp/>
-
- 
- {movies.map((movie)=>(
-  <article key={movie.id}> 
-  <strong>{movie.title}</strong>
-  <img className={styles.img} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
-  <Link to={`/movie/${movie.id}`}>Acessar</Link>
-  </article>
-  ))}
-    </>
-  )
-}
+ function App() {
+return(
+  <>
+  <Header/>
+       <Routes>
+        <Route exact path="*" element={<Home />} />
+        <Route exact path="/movie/:id" element={<Movie />} />
+      
+      </Routes>
+  </>
+)}
 
 export default App
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,40 @@
+import {useEffect, useState} from 'react'
+import api from '../../services/api'
+import {Link} from "react-router-dom";
+import styles from './Home.module.css'
 
 function Home() {
-  
+  const [movies, setMovies] = useState<MoviesProps[]>([])
+
+  useEffect(()=>{
+    async function loadMovies() {
+     const response = await api.get("movie/now_playing", {
+      params: {
+      api_key: "52139d7800b359f24813bae453e23e2a",
+     language: "pt-BR",
+     page: 1,
+     }
+   })
+   setMovies(response.data.results)
+  }
+    loadMovies();
+  },[])
   return (
-    <>
-    <h1>Filmes carregando</h1>
+    <> 
+      
+ 
+      {movies.map((movie) => {
+            return (
+              <article className={styles.container} key={movie.id}>
+                <strong className={styles.title}>{movie.title}</strong>
+                <img className={styles.img}
+                  src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                  alt={movie.title}
+                />
+                <Link to={`/movie/${movie.id}`} className={styles.link}>Acessar</Link>
+              </article>
+            );
+          })} 
     </>
   )
 }
